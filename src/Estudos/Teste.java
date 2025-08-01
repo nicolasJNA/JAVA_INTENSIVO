77dd66af16e3884ad1821610a222f0788a777d70
Nicolas <nicolasjnascimento1002@gmail.com>
2025-08-01 10:20:43 -0300

    Teste

---
+++ src/Estudos/Teste.java
@@ -0,0 +1,8 @@
+package Estudos;
+
+public class Teste {
+public static void main (String args[]) {
+	Funcionario funcionario = new Funcionario("Ncolas","85633625",50000);
+	System.out.print(funcionario.getSalario());
+	}
+}
